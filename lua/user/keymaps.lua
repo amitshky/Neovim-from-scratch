7336438e076b7e5f56cdfc0a85c5cb4475518a7b
amitshky <a7shky@gmail.com>
2022-12-30 11:22:34 +0545

    update

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -32,8 +32,8 @@ keymap("n", "<C-Right>", "<C-w>l", opts)
 --keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<S-Right>", ":bnext<CR>", opts)
+keymap("n", "<S-Left>", ":bprevious<CR>", opts)
 
 -- Move text up and down
 --keymap("n", "<A-Down>", "<Esc>:m .+1<CR>==gi", opts)
